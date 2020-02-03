27e5f49be32fbefd4977d6915c1d610b2a4d53fe
melanie-taing <>
2020-02-02 23:58:09 -0500

    Example of post method

--- src/Httpc.java
+++ src/Httpc.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.util.Scanner;
+
 public class Httpc {
 
-    public void init() {}
     public Httpc() {
         init();
     }
@@ -31,18 +35,53 @@ public class Httpc {
         System.out.println("Httpc terminated successfully");
     }
 
-    // httpc get [-v] [-h key:value] URL
-    public void GET(String[] input) {
+    // get [-v] [-h key:value] URL
+    private void GET(String input) {
         System.out.println("GET METHOD");
     }
 
-    // httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL
-    public void POST(String[] input) {
+    // post [-v] [-h key:value] [-d inline-data] [-f file] URL
+    private void POST(String input) {
         System.out.println("POST METHOD");
+
         // Process input
+
+        // Create socket with URL and Port
+        String host = "httpbin.org";
+        int port = 80;
+        String body = "key1=value1&key2=value2";
+        String request = "POST /post HTTP/1.0\r\n"
+                + "Content-Type:application/x-www-form-urlencoded\r\n"
+                + "Content-Length: " + body.length() +"\r\n"
+                + "\r\n"
+                + body;
+
+        try {
+            Socket socket = new Socket(host, port);
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            outputStream.write(request.getBytes());
+            outputStream.flush();
+
+            StringBuilder response = new StringBuilder();
+
+            int data = inputStream.read();
+
+            while(data != -1) {
+                response.append((char) data);
+                data = inputStream.read();
+            }
+
+            System.out.println(response);
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void HELP(String input) {
+    private void HELP(String input) {
         if (input.contains("get")) {
             System.out.println("Usage: httpc get [-v] [-h key:value] URL" +
                     "\n\n" + "Get executes a HTTP GET request for a given URL. " +
