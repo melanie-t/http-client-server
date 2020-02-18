e9d0ae369bd327e034efe76dbec2470890250df1
melanie-t <melanie.taing@mail.concordia.ca>
2020-02-18 12:38:31 -0500

    Added sample commands for file server and data files

--- src/Httpfs.java
+++ src/Httpfs.java
@@ -5,35 +5,50 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Httpfs {
+
+    final int DEFAULT_PORT = 8080;
+
     public Httpfs() {
         init();
     }
 
     private void init() {
-        System.out.println("Welcome to httpfs. To quit the application, enter /q");
+        // Default port is 8080 if not specified
+        int port_number = DEFAULT_PORT;
 
+        System.out.print("Welcome to httpfs." +
+                "\nEnter a port number to initialize the server (default port is 8080): ");
         Scanner kb = new Scanner(System.in);
-        boolean running = true;
-        while (running) {
+        String input = kb.next();
 
-            System.out.print("httpfs> ");
-            String requestType = kb.next();
-            String input = kb.nextLine().trim();
-
-            if (requestType.equalsIgnoreCase("/q")) {
-                running = false;
-            } else if (requestType.equalsIgnoreCase("help")) {
-                HELP();
-            }
+        if (!input.isEmpty()) {
+            port_number = Integer.valueOf(input);
         }
-        System.out.println("Httpfs terminated successfully");
+
+        // Start server
+        server_socket(port_number);
     }
 
-    private void server_socket(int portNumber) throws IOException {
-        ServerSocket socket = new ServerSocket(portNumber);
-        Socket client = socket.accept();
-        PrintWriter out = new PrintWriter(client.getOutputStream());
+    private void server_socket(int server_port) {
 
+        // Source: https://github.com/SebastienBah/COMP445TA/blob/master/Lab02/httpfs/httpfs.java
+        try (ServerSocket server = new ServerSocket(server_port)) {
+            System.out.println("Server has been instantiated at port " + server_port);
+            // Server initialized and waits for client requests
+            while(true) {
+                // Is this a blocking or non-blocking call?
+                // What would you need to do to service multiple clients at the same time?
+                try (Socket client_connection = server.accept()) {
+                    PrintWriter outbount_client = new PrintWriter(client_connection.getOutputStream(), true);
+                    outbount_client.println("Well hello to you too.");
+                    client_connection.close();
+                    System.out.println("Httpfs terminated successfully");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // End source
     }
 
     private void HELP() {
