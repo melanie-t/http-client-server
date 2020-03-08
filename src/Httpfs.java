ed2ed33063bade4b29a8137eb824845c77494795
Ziad Hawa <ziadhawah27@gmail.com>
2020-03-07 19:57:29 -0500

    done with get stuff added ability to use -v -p and -d in httpfs

--- src/Httpfs.java
+++ src/Httpfs.java
@@ -1,11 +1,13 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Httpfs {
 
-    final int DEFAULT_PORT = 8080;
+    final int DEFAULT_PORT = 8081;
     final String DEFAULT_DIRECTORY = "data/";
 
     public Httpfs() {
@@ -16,7 +18,7 @@ public class Httpfs {
         // Default port is 8080 if not specified
         int port_number = DEFAULT_PORT;
         String directory = null;
-        boolean verbose = true;
+        boolean verbose = false;
 
         System.out.println("Welcome to httpfs. Usage: httpfs [-v] [-p PORT] [-d PATH-TO-DIR]");
         Scanner kb = new Scanner(System.in);
@@ -25,17 +27,28 @@ public class Httpfs {
         while (true) {
             System.out.print("httpfs > ");
             String input = kb.nextLine().toLowerCase().trim();
-
+            String[] inputSplit = input.split(" ");
             if (input.contains("/q"))
                 break;
 
             // Parse verbose, port and directory
             if (input.contains("-p")) {
-                // port_number = Integer.valueOf(input);
+                for(int i = 0; i < inputSplit.length; i++){
+                    if(inputSplit[i].equalsIgnoreCase("-p")){
+                        port_number = Integer.parseInt(inputSplit[i+1]);
+                    }
+                }
             }
 
             if (input.contains("-d")) {
-
+                for(int i = 0; i < inputSplit.length; i++){
+                    if(inputSplit[i].equalsIgnoreCase("-d")){
+                        directory = inputSplit[i+1];
+                        if(directory.charAt(directory.length()-1) != '/'){
+                            directory += "/";
+                        }
+                    }
+                }
             }
 
             if (input.contains("-v")) {
@@ -105,7 +118,8 @@ public class Httpfs {
                             // TODO Ziad: Append the data (ex: Opening data/data_1.txt) to response
                             //  String data = GET(requestType);
                             //  response.append(data)
-                            GET(requestType);
+                            String get = GET(requestLine, directory, httpVersion, userAgent, verbose);
+                            response.append(get);
                             }
 
                         // TODO Melanie
@@ -132,56 +146,87 @@ public class Httpfs {
         // End source
     }
 
-    private void GET(String input){
-        boolean verbose = false;
-        if (input.contains("-v")) {
-            input = input.replace("-v", "").trim();
-            verbose = true;
-        }
+    private String GET(String input, String directory, String httpVersion, String userAgent, boolean verbose) {
+        StringBuilder returned = new StringBuilder();
         String[] inputDivided = input.split(" ");
 
         if (input.contains("/")){
             if((input.charAt(input.length() - 1) == '/') || (input.charAt(input.indexOf("/") + 1) == ' ')) {
-                listFiles("data");
-            } else {
-                String fileName = inputDivided[1];
-                boolean readable = false;
-                String fileToOpen = "data" + fileName;
-                while (!readable) {
-                    if (fileToOpen.contains(".")) {
-                        readable = true;
-                    } else {
-                        listFiles(fileToOpen);
-                        System.out.print("File specified was a directory, choose a file name within this directory to read: ");
-                        Scanner kb = new Scanner(System.in);
-                        String userInput = kb.nextLine().trim();
-                        fileToOpen += "/" + userInput;
-                    }
+                if (verbose) {
+                System.out.println("Processing GET request");
+                System.out.println(input);
+                System.out.println("Showing contents of directory: " + directory);
                 }
-                System.out.println("Opening " + fileToOpen);
+                returned.append(listFiles(directory, httpVersion, userAgent, verbose));
+            } else {
+                String fileName = inputDivided[1].substring(1);
+                String fileToOpen = directory + fileName;
+                if (fileToOpen.contains(".")) {
+                    String wholeText= "";
+                    if (verbose) {
+                        System.out.println("Processing GET request");
+                        System.out.println(input);
+                        System.out.println("Opening file: " + fileToOpen);
+                    }
+                    try {
+                        wholeText = new String(Files.readAllBytes(Paths.get(fileToOpen)));
+                        if (verbose) {
+                            returned.append("\n" + httpVersion + " 200 OK " + "\n" + userAgent);
+                        }
+                        returned.append("\n" + wholeText);
+                    } catch (IOException o){
+                        System.out.println("File cannot be opened/has no content");
+                        returned.append("\n" + httpVersion + " 404 Not found " + "\n" + userAgent);
+                    }
+
+                } else {
+                    if (verbose) {
+                        System.out.println("Processing GET request");
+                        System.out.println(input);
+                        System.out.println("File to open was a folder named: " + fileToOpen);
+                        System.out.println("Showing folder contents to client...");
+                    }
+                    returned.append("Opening Directory: " + fileToOpen + "\n");
+                    returned.append(listFiles(fileToOpen, httpVersion, userAgent,verbose));
+                }
             }
 
         }
+        return returned.toString();
     }
-    private void listFiles(String parent){
+    private String listFiles(String parent, String httpVersion, String userAgent, boolean verbose){
+        StringBuilder returned = new StringBuilder();
         File dataDir = new File(parent);
         File[] filesInDirectory = dataDir.listFiles();
-        System.out.println("File/Directory Name         Type");
-        if(filesInDirectory.length != 0){
-            for (int i = 0; i < filesInDirectory.length; i++){
-                String fileNameComplete = filesInDirectory[i].getName();
-                String fileName;
-                String fileType;
-                if(fileNameComplete.contains(".")) {
-                    fileName = fileNameComplete.substring(0, fileNameComplete.lastIndexOf('.'));
-                    fileType = fileNameComplete.substring(fileNameComplete.lastIndexOf('.'));
-                } else {
-                    fileName = fileNameComplete.trim();
-                    fileType = "\tFolder";
+        if(filesInDirectory != null){
+            if(filesInDirectory.length != 0){
+                if (verbose) {
+                returned.append("\n" + httpVersion + " 200 OK " + "\n" + userAgent);
                 }
-                System.out.println(fileName + "\t\t\t\t\t\t" + fileType); //FilenameUtils.getExtension(filesInDirectory[i])
+                returned.append("File/Directory Name\t\t\t\tType\n");
+                for (int i = 0; i < filesInDirectory.length; i++){
+                    String fileNameComplete = filesInDirectory[i].getName();
+                    String fileName;
+                    String fileType;
+                    if(fileNameComplete.contains(".")) {
+                        fileName = fileNameComplete.substring(0, fileNameComplete.lastIndexOf('.'));
+                        fileType = fileNameComplete.substring(fileNameComplete.lastIndexOf('.'));
+                    } else {
+                        fileName = fileNameComplete.trim();
+                        fileType = "Folder";
+                    }
+                    returned.append(fileName).append("\t\t\t\t\t\t").append(fileType).append("\n"); //FilenameUtils.getExtension(filesInDirectory[i])
+                }
+
+            } else returned.append("Directory is empty");
+        } else {
+            if (verbose){
+                returned.append("\n" + httpVersion + " 404 Not found " + "\n" + userAgent);
             }
+            returned.append("\nDirectory not found\n");
+
         }
+        return returned.toString();
     }
 
     private String POST(String directory, String httpVersion, String userAgent, String requestLine, String body, boolean verbose) {
